41e69f938db8fbb00fba543bebdd65cdc3fe759e
Greg Pfeil <greg@technomadic.org>
2025-09-08 16:17:38 -0600

    Support versions of Autodocodec < 0.4

--- core/src/Autodocodec/Dhall/Decode.hs
+++ core/src/Autodocodec/Dhall/Decode.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE Trustworthy #-}
 {-# OPTIONS_GHC -Wwarn=incomplete-patterns #-}
@@ -19,9 +20,8 @@ import safe "base" Data.Either (Either (Left), either)
 import safe "base" Data.Eq ((==))
 import safe "base" Data.Function (const, ($))
 import safe "base" Data.Functor (fmap, (<$>))
-import safe "base" Data.Maybe (maybe)
+import safe "base" Data.Maybe (Maybe, maybe)
 import safe "base" Data.Semigroup ((<>))
-import safe "base" Data.Tuple (snd)
 import safe "base" Data.Void (Void)
 import safe "base" Text.Show (show)
 import qualified "dhall" Dhall.Marshal.Decode as Dhall
@@ -32,8 +32,12 @@ import qualified "dhall" Dhall.Marshal.Decode as Dhall
 import qualified "dhall" Dhall.Marshal.Decode as Dhall.Decode
 import qualified "dhall" Dhall.Src as Dhall (Src)
 import qualified "either" Data.Either.Validation as Validation
+import safe "text" Data.Text (Text)
 import safe qualified "text" Data.Text as Text
+#if MIN_VERSION_autodocodec(0, 2, 0)
+import safe "base" Data.Tuple (snd)
 import safe qualified "unordered-containers" Data.HashMap.Strict as HashMap
+#endif
 
 parseViaCodec :: (Autodo.HasCodec a) => Dhall.Decoder a
 parseViaCodec = parseVia Autodo.codec
@@ -43,7 +47,6 @@ parseVia = \case
   Autodo.NullCodec -> coerce <$> Dhall.Decode.unit
   Autodo.BoolCodec _ -> coerce <$> Dhall.Decode.bool
   Autodo.StringCodec _ -> coerce <$> Dhall.Decode.strictText
-  Autodo.IntegerCodec _ _ -> coerce <$> Dhall.Decode.integer
   Autodo.NumberCodec _ _ -> coerce <$> Dhall.Decode.scientific
   -- Autodo.HashMapCodec v -> coerce <$> Dhall.Decode.hashMap (Compat.toKey <$> Dhall.Decode.strictText) (parseVia v)
   -- Autodo.MapCodec v -> coerce <$> Dhall.Decode.map (Dhall.Decode.strictText) (parseVia v)
@@ -76,6 +79,9 @@ parseVia = \case
         <> Dhall.Decode.constructor "Right" (pure <$> parseVia r)
   Autodo.CommentCodec _ c -> coerce <$> parseVia c
   Autodo.ReferenceCodec _ c -> coerce <$> parseVia c
+#if MIN_VERSION_autodocodec(0, 4, 0)
+  Autodo.IntegerCodec _ _ -> coerce <$> Dhall.Decode.integer
+#endif
 
 -- | Like `=<<` for `Dhall.Decoder`.
 bindExtractor ::
@@ -108,9 +114,6 @@ parseObjectVia' = \case
     Left . fmap coerce $
       Dhall.Decode.constructor "Left" (Left <$> parseObjectVia l)
         <> Dhall.Decode.constructor "Right" (pure <$> parseObjectVia r)
-  Autodo.DiscriminatedUnionCodec _ _ u ->
-    Left . fmap coerce $
-      HashMap.foldMapWithKey (\k -> Dhall.Decode.constructor k . parseObjectVia . snd) u
   Autodo.RequiredKeyCodec k v _ -> pure . fmap coerce . Dhall.Decode.field k $ parseVia v
   Autodo.OptionalKeyCodec k v _ -> pure $ coerce <$> parseOptionalField k v
   Autodo.OptionalKeyWithDefaultCodec k v def _ ->
@@ -118,6 +121,15 @@ parseObjectVia' = \case
   Autodo.OptionalKeyWithOmittedDefaultCodec k v def _ ->
     pure $ maybe (coerce def) coerce <$> parseOptionalField k v
   Autodo.PureCodec a -> pure $ pure a
-  -- Autodo.ApCodec f a -> pure $ parseObjectVia f <*> parseObjectVia a
-  where
-    parseOptionalField k = Dhall.Decode.field k . Dhall.Decode.maybe . parseVia
+
+-- Autodo.ApCodec f a -> pure $ parseObjectVia f <*> parseObjectVia a
+#if MIN_VERSION_autodocodec(0, 2, 0)
+  Autodo.DiscriminatedUnionCodec _ _ u ->
+    Left . fmap coerce $
+      HashMap.foldMapWithKey
+        (\k -> Dhall.Decode.constructor k . parseObjectVia . snd)
+        u
+#endif
+
+parseOptionalField :: Text -> Autodo.ValueCodec void a -> Dhall.RecordDecoder (Maybe a)
+parseOptionalField k = Dhall.Decode.field k . Dhall.Decode.maybe . parseVia
