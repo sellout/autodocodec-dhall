d7f35c484b4a3b33edeeb0d5ccdca91a4f160505
Greg Pfeil <greg@technomadic.org>
2025-09-09 07:37:01 -0600

    Punt on GHC < 8.6
    None of these are passing, and I don’t expect demand for it.

--- flake.nix
+++ flake.nix
@@ -140,10 +140,11 @@
         ## The versions that are older than those supported by Nix that we
         ## prefer to test against.
         nonNixTestedGhcVersions = [
-          "7.10.3"
-          "8.0.2"
-          "8.2.2"
-          "8.4.1"
+          ## For now, not supporting older versions, since nothing passes.
+          # "7.10.3"
+          # "8.0.2"
+          # "8.2.2"
+          # "8.4.1"
           "8.6.1"
           "8.8.1"
           "8.10.1"
